7cec7536c667c8f4584595fdd2a37873c70d73c7
James Serengia <jamesserengia@gmail.com>
2023-11-28 13:07:15 +0300

    Implement insert method

--- main.js
+++ main.js
@@ -10,4 +10,56 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  //   INSERTING A NODE IN BST
+  // # start on the root node, check is the value > than root
+  //         if val > root node .val
+  //   Go to the right, check if there if value, if there is no node,
+  //   that is the new home, other wise repeat the process
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (value < current.value) {
+        // move to the left
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else if (value > current.value) {
+        // Move right
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right;
+        }
+      } else {
+        return undefined;
+      }
+    }
+  }
 }
+
+const BST = new BinarySearchTree();
+//             10
+//       5             13
+//  2        7     11       16
+
+BST.insert(10);
+BST.insert(5);
+BST.insert(2);
+BST.insert(7);
+BST.insert(13);
+BST.insert(11);
+BST.insert(16);
+
+console.log(JSON.stringify(BST));
